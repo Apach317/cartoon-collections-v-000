84c4e5ab1663c3cccc46c7601aa29d3711f431b0
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 20:53:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(long_calls)
-  long_calls.all? = lc
+  long_calls.all? == lc
     if lc.length > 4
       return false
     else
